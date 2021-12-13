d88b1795d8933ef23717d898daa7fc45afe84304
Abdullateef Ademoye <47930539+abdullateefakorede@users.noreply.github.com>
2021-12-13 13:11:48 +0100

    Revert "Feature unit test"

--- src/helpers/error.js
+++
@@ -1,7 +0,0 @@
-const responseHelper = require('./response');
-
-exports.handleError = (err, res) => {
-	console.error('Error: ', err);
-	const msg = typeof err === 'string' ? err : 'SOMETHING_WENT_WRONG';
-	return responseHelper.sendErrorResponse(res, {}, msg);
-};
